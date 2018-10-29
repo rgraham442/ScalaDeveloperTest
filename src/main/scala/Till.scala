8e3bdb9a1ec6f6fb1444a931e141d350934d20d5
robyng442 <robjgraham@gmail.com>
2018-10-29 15:41:02 +0000

    Step 2 - discounts available (with more main printlines in main

--- src/main/scala/Till.scala
+++ src/main/scala/Till.scala
@@ -1,6 +1,7 @@
 object Till {
   def main(args: Array[String]): Unit = {
     println(new ShoppingCart checkout (new Apple addFruit  1, new Orange addFruit 1))
-
+    println(new ShoppingCart checkout (new Apple addFruit  2, new Orange addFruit 3))
+    println(new ShoppingCart checkout (new Apple addFruit  7, new Orange addFruit 5))
   }
 }
